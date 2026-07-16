6a4fa5b659000ded0a59f4161c56a761f66ec5c7
Mihanyy <123839146+Mihanyy@users.noreply.github.com>
2026-07-16 11:56:42 +0300

    1 задание (правки)

--- src/main/resources/db/migration/V1__bb.sql
+++ src/main/resources/db/migration/V1__bb.sql
@@ -1,26 +1,26 @@
 -- Таблица пользователей
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL CHECK (btrim(name) <> ''),
-    login VARCHAR(50)  NOT NULL UNIQUE CHECK (char_length(login) >= 3),
-    email VARCHAR(255) NOT NULL UNIQUE CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
-    password_hash VARCHAR(255) NOT NULL CHECK (char_length(password_hash) >= 20),
-    role VARCHAR(10) NOT NULL DEFAULT 'user' CHECK (role IN ('user', 'admin')),
-    is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+            id BIGSERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            login VARCHAR(50)  NOT NULL UNIQUE,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            password_hash VARCHAR(255) NOT NULL,
+            role VARCHAR(10) NOT NULL,
+            is_blocked BOOLEAN NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 -- Таблица объявлений
 CREATE TABLE listings (
-    id  BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    category VARCHAR(100) NOT NULL CHECK (btrim(category) <> ''),
-    title VARCHAR(150) NOT NULL CHECK (btrim(title) <> ''),
-    description TEXT,
-    price NUMERIC(12, 2) NOT NULL CHECK (price >= 0),
-    status VARCHAR(25) NOT NULL DEFAULT 'active' CHECK (status IN ('active', 'deactivated_by_owner', 'deactivated_by_admin')),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+            id  BIGSERIAL PRIMARY KEY,
+            user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+            category VARCHAR(100) NOT NULL,
+            title VARCHAR(150) NOT NULL,
+            description TEXT,
+            price VARCHAR(100) NOT NULL,
+            status VARCHAR(25) NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+            updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 -- Индексы
@@ -42,6 +42,6 @@ END;
 $$;
 
 CREATE TRIGGER trg_listings_updated_at
-BEFORE UPDATE ON listings
-FOR EACH ROW
-EXECUTE FUNCTION set_updated_at();
+    BEFORE UPDATE ON listings
+    FOR EACH ROW
+    EXECUTE FUNCTION set_updated_at();
